77ff82de2e4385329cba75b33d561003e1e991f6
leticiarosadominguescontato@gmail.com <leticiardominguescontato@gmail.com>
2023-03-19 22:46:50 -0300

    primeira versao

---
+++ funcionamento.js
@@ -0,0 +1,23 @@
+
+function calcular(){
+
+    var peso, altura;
+
+    peso = parseFloat(document.getElementById("peso").value);
+    altura = parseFloat(document.getElementById("altura").value);
+
+    
+
+
+
+}
+
+function limpar(){
+    peso = parseFloat(document.getElementById("peso").value);
+    altura = parseFloat(document.getElementById("altura").value);
+    resultado = parseFloat(document.getElementById("resposta").value);
+
+    document.getElementById("peso").value = " ";
+    document.getElementById("altura").value = " ";
+    document.getElementById("resposta").value = " ";
+}
