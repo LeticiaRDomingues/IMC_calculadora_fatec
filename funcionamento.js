2692eef53567f0c9d9a13b3b860f021fb137adba
LeticiaRDomingues <lerosacontato@gmail.com>
2023-03-21 10:13:56 -0300

    versão dois

--- funcionamento.js
+++ funcionamento.js
@@ -1,23 +1,26 @@
 
 function calcular(){
+  var  peso = parseFloat(document.getElementById("peso").value);
+    var altura = parseFloat(document.getElementById("altura").value);
+    var resultado = document.getElementById("resultado");
 
-    var peso, altura;
+    calculo = peso/(altura*altura);
 
-    peso = parseFloat(document.getElementById("peso").value);
-    altura = parseFloat(document.getElementById("altura").value);
+   
+    if(calculo>=25){
+        resultado.innerHTML = "normal";
+    }
 
-    
-
-
-
+console.log(calculo);
 }
 
 function limpar(){
     peso = parseFloat(document.getElementById("peso").value);
     altura = parseFloat(document.getElementById("altura").value);
-    resultado = parseFloat(document.getElementById("resposta").value);
+    resultado = parseFloat(document.getElementById("resultado").value);
+
 
     document.getElementById("peso").value = " ";
     document.getElementById("altura").value = " ";
-    document.getElementById("resposta").value = " ";
+    document.getElementById("resultado").value = " ";
 }
